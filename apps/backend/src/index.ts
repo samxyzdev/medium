1a733ab98ab20fceef466a93d776cf03662640b7
Sameer Ahmed <123456789+odin@users.noreply.github.com>
2025-05-15 21:53:17 +0530

    created signup and signin router

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,19 +2,74 @@ import express from "express";
 import { prisma } from "@repo/db/prisma";
 import { authMiddleware } from "./middleware.js";
 import { UserSchema, BlogSchema } from "@repo/db/schema";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const app = express();
 
-app.get("/signup", (req, res) => {
-  const data = UserSchema.safeParse(req.body);
-  if (!data.success) {
+app.get("/signup", async (req, res) => {
+  const userData = UserSchema.safeParse(req.body);
+  if (!userData.success) {
     res.json({
       msg: "Please send correct data",
     });
     return;
   }
+  const hashedPassword = await bcrypt.hash(userData.data.password, 10);
+  const createUser = await prisma.user.create({
+    data: {
+      username: userData.data.username,
+      password: hashedPassword,
+      role: userData.data.role,
+    },
+  });
+  console.log(process.env.JWT_SECRET);
+  const token = jwt.sign({ userId: createUser.id }, process.env.JWT_SECRET!);
+  res.json({
+    msg: "User created succesfully",
+    token: token,
+  });
+  return;
 });
-app.get("/signin", (req, res) => {});
+
+app.get("/signin", async (req, res) => {
+  const userData = UserSchema.safeParse(req.body);
+  if (!userData.success) {
+    res.json({
+      msg: "Please send correct data",
+    });
+    return;
+  }
+
+  const isUserExist = await prisma.user.findFirst({
+    where: {
+      username: userData.data.username,
+    },
+  });
+  if (!isUserExist) {
+    res.json({
+      msg: "User doesn't exist",
+    });
+    return;
+  }
+  const comparePassword = await bcrypt.compare(
+    userData.data.password,
+    isUserExist.password
+  );
+  if (!comparePassword) {
+    res.json({
+      msg: "Please enter correct password",
+    });
+    return;
+  }
+  console.log(process.env.JWT_SECRET);
+  const token = jwt.sign({ userId: isUserExist.id }, process.env.JWT_SECRET!);
+  res.json({
+    msg: "User created succesfully",
+    token: token,
+  });
+  return;
+});
 app.post("/admin/blog", authMiddleware, (req, res) => {});
 app.get("/user/:blog", (req, res) => {});
 app.get("/top10blog", (req, res) => {});
